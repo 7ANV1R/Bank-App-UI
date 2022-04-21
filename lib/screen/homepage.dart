4b6110c64428f3f1aeb5ced41e2bd008594e79f4
Tanvir <ibnmizan7878@gmail.com>
2022-04-21 09:39:10 +0600

    commiting a bug

--- lib/screen/homepage.dart
+++ lib/screen/homepage.dart
@@ -1,3 +1,4 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -8,6 +9,26 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final headerNotifier = ValueNotifier<_MyHeader?>(null);
+
+  void _refreshHeader(String title, bool visible, {String? lastOne}) {
+    final headerValue = headerNotifier.value;
+    final headerTitle = headerValue?.title ?? title;
+    final headerVisible = headerValue?.visible ?? visible;
+
+    //TODO: Review this if else logic
+
+    if (headerTitle != title || headerVisible != visible || lastOne != null) {
+      Future.microtask(() {
+        if (!visible && lastOne != null) {
+          headerNotifier.value = _MyHeader(title: lastOne, visible: true);
+        } else {
+          headerNotifier.value = _MyHeader(title: title, visible: visible);
+        }
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,7 +49,9 @@ class _HomePageState extends State<HomePage> {
                 ),
                 ...[
                   SliverPersistentHeader(
-                    delegate: _MyHeaderTitle((visible) {}, 'This'),
+                    delegate: _MyHeaderTitle((visible) {
+                      _refreshHeader('April', visible);
+                    }, 'Latest Transaction'),
                   ),
                   SliverList(
                       delegate: SliverChildBuilderDelegate(
@@ -42,11 +65,121 @@ class _HomePageState extends State<HomePage> {
                         ),
                       );
                     },
-                    childCount: 30,
+                    childCount: 10,
+                  ))
+                ],
+                ...[
+                  SliverPersistentHeader(
+                    delegate: _MyHeaderTitle((visible) {
+                      _refreshHeader('March', visible, lastOne: 'April');
+                    }, 'March'),
+                  ),
+                  SliverList(
+                      delegate: SliverChildBuilderDelegate(
+                    (context, index) {
+                      return ListTile(
+                        title: Text(
+                          'Title : $index',
+                          style: const TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                      );
+                    },
+                    childCount: 10,
+                  ))
+                ],
+                ...[
+                  SliverPersistentHeader(
+                    delegate: _MyHeaderTitle((visible) {
+                      _refreshHeader('February', visible, lastOne: 'March');
+                    }, 'February'),
+                  ),
+                  SliverList(
+                      delegate: SliverChildBuilderDelegate(
+                    (context, index) {
+                      return ListTile(
+                        title: Text(
+                          'Title : $index',
+                          style: const TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                      );
+                    },
+                    childCount: 10,
+                  ))
+                ],
+                ...[
+                  SliverPersistentHeader(
+                    delegate: _MyHeaderTitle((visible) {
+                      _refreshHeader('January', visible, lastOne: 'February');
+                    }, 'January'),
+                  ),
+                  SliverList(
+                      delegate: SliverChildBuilderDelegate(
+                    (context, index) {
+                      return ListTile(
+                        title: Text(
+                          'Title : $index',
+                          style: const TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                      );
+                    },
+                    childCount: 10,
+                  ))
+                ],
+                ...[
+                  SliverPersistentHeader(
+                    delegate: _MyHeaderTitle((visible) {
+                      _refreshHeader('December', visible, lastOne: 'January');
+                    }, 'December'),
+                  ),
+                  SliverList(
+                      delegate: SliverChildBuilderDelegate(
+                    (context, index) {
+                      return ListTile(
+                        title: Text(
+                          'Title : $index',
+                          style: const TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                      );
+                    },
+                    childCount: 20,
                   ))
                 ]
               ],
             ),
+            ValueListenableBuilder<_MyHeader?>(
+                valueListenable: headerNotifier,
+                builder: (context, snapshot, _) {
+                  final visible = snapshot?.visible ?? false;
+                  final title = snapshot?.title ?? '';
+                  return Positioned(
+                    left: 15,
+                    right: 0,
+                    top: 0,
+                    child: visible
+                        ? DecoratedBox(
+                            decoration: BoxDecoration(
+                              color: Colors.black.withOpacity(0.7),
+                            ),
+                            child: Text(
+                              title,
+                              style: const TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.w400,
+                                color: Colors.white,
+                              ),
+                            ),
+                          )
+                        : const SizedBox.shrink(),
+                  );
+                }),
             Positioned(
               right: 10,
               top: 0,
@@ -62,6 +195,15 @@ class _HomePageState extends State<HomePage> {
   }
 }
 
+class _MyHeader {
+  const _MyHeader({
+    required this.title,
+    required this.visible,
+  });
+  final String title;
+  final bool visible;
+}
+
 const maxHeaderTitleHeight = 55.0;
 typedef OnHeaderChanged = void Function(bool visible);
 
@@ -72,14 +214,23 @@ class _MyHeaderTitle extends SliverPersistentHeaderDelegate {
 
   @override
   Widget build(BuildContext context, double shrinkOffset, bool overlapsContent) {
-    return Align(
-      alignment: Alignment.centerLeft,
-      child: Text(
-        title,
-        style: const TextStyle(
-          fontSize: 20,
-          fontWeight: FontWeight.w400,
-          color: Colors.white,
+    if (shrinkOffset > 0) {
+      onHeaderChanged(true);
+    } else {
+      onHeaderChanged(false);
+    }
+
+    return Padding(
+      padding: const EdgeInsets.only(left: 16.0),
+      child: Align(
+        alignment: Alignment.centerLeft,
+        child: Text(
+          title,
+          style: const TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.w600,
+            color: Colors.white,
+          ),
         ),
       ),
     );
